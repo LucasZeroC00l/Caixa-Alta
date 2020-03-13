ea357a2617593e19b7281831d856a97991a3117e
LucasZeroCool <lucas.10.nunes@live.com>
2020-03-13 20:23:15 -0300

    Criei um programa.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Caixa_Alta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Escreva algo aqui: ");
+            string algo;
+            algo = Console.ReadLine();
+            Console.WriteLine(algo.ToUpper());
+            Console.WriteLine("Conseguiu, miseravi!");
+        
+        }
+    }
+}
